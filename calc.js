b90f5e6d212f03205fca542ef865c410502b0f34
vin_3462 <52646222+vincentlee3462@users.noreply.github.com>
2024-01-07 01:59:04 +0800

    Update calc.js

--- calc.js
+++ calc.js
@@ -53,7 +53,7 @@ function num_click() {
   console.log("before", nums);
   nums[num_count] += this.id;
   console.log("after", nums);
-
+  console.log(num_count);
 }
 
 function operator_click() {
@@ -61,19 +61,30 @@ function operator_click() {
 
   if (operate.innerHTML != "None") {
     if (last == "+" || last == "-" || last == "*" || last == "/") {
+      console.log("if");
       //if already have operator, then remove the exist one, and add the new one
       //"3+" => "3-"
       operate.innerHTML = operate.innerHTML.slice(0, -1);
   
-      //also change operator in the "oper" array
+      //also change operator in the "nums" array
       nums[num_count-1] = this.id;
       console.log(nums);
-    } else if (this.id == "-" && !(nums[num_count-1] > "0" && nums[num_count-1] < "9")) {
+    } else if (this.id == "-" && !(nums[num_count-1] >= "0" && nums[num_count-1] <= "9")) {
       //num_count -= 1;
-      nums[num_count] += this.id;
+      console.log("else if");
+      if (nums[num_count] == "") {
+        nums[num_count] += this.id;
+      } else {
+        nums.push(this.id);
+        nums.push("");
+        num_count += 2;
+      }
+    } else if (nums[num_count] == "") {
+      nums[num_count] = this.id;
+      console.log(num_count);
     } else {
       //num_count++;
-      
+      console.log("else");
       num_count++;
       nums[num_count] = this.id;
       num_count++;
@@ -85,6 +96,8 @@ function operator_click() {
   } else if (this.id == "-") {
     nums[num_count] += this.id;
   }
+  console.log(num_count);
+  console.log(nums);
 }
 
 function symbol_click() {
@@ -97,28 +110,30 @@ function symbol_click() {
     //dot symbol
     if (nums[num_count].length == 0) {
       operate.innerHTML += "0.";
-      nums[num_count] += "0."
-      console.log(nums[num_count]);
+      nums[num_count] += "0.";
+      
     } else if (nums[num_count].includes(".") == false) {
       operate.innerHTML += ".";
       nums[num_count] += ".";
     } 
   } else if (this.id == "open") {
-    //open blanket
+    //open bracket
     if (nums[num_count] != "") {
       num_count++;
       nums[num_count] = "";
     }
-    if (nums[num_count-1] > "0" && nums[num_count-1] < "9") {
+    if (nums[num_count-1] >= "0" && nums[num_count-1] <= "9") {
       nums[num_count] = "*";
       num_count++;
       nums[num_count] = "";
     }
+    console.log(nums);
     nums[num_count] = "(";
     open_blan.push(num_count);
     num_count++;
     nums[num_count] = "";
     operate.innerHTML += "(";
+    console.log("open bracket", open_blan);
   } else if (this.id == "close") {
     if (close_blan.length < open_blan.length) {
       if (nums[num_count] != "") {
@@ -130,10 +145,12 @@ function symbol_click() {
       num_count++;
       nums[num_count] = "";
       operate.innerHTML += ")";
-      
+      console.log("close bracket", close_blan);
     }
   }
   console.log(nums);
+  console.log(num_count);
+
 }
 
 function action_click() {
@@ -142,18 +159,144 @@ function action_click() {
     //oper = [];
     num_count = 0;
     //oper_count = 0;
+    open_blan = [];
+    close_blan = [];
 
     operate.innerHTML = "None";
     operate.style.visibility = "hidden";
     result.innerHTML = "0";
     answer = result.innerHTML;
+  } else if (this.id == "result") {
+    
+    if (nums.at(-1) == "") {
+      nums.pop();
+    }
+
+    if (nums.at(-1) == "(") {
+      nums.pop();
+      open_blan.pop();
+    }
+
+    if (nums.at(-1) == "+" || nums.at(-1) == "-" || nums.at(-1) == "*" || nums.at(-1) == "/") {
+      nums.pop();
+    }
+
+    console.log(nums);
+    console.log(open_blan);
+    if (close_blan.length < open_blan.length) {
+      console.log("open bracket", open_blan);
+      console.log("close bracket", close_blan);
+      add_close = open_blan.length - close_blan.length;
+      for (var i=0; i<add_close; i++) {
+        console.log(i);
+        nums.push(")");
+        num_count = nums.length-1;
+        close_blan.push(num_count);
+      }
+    }
+    
+    //turn number string to numbers
+    for (var i=0; i<nums.length; i++) {
+      if (nums[i] >= "0" && nums[i] <= "9") {
+        nums[i] = Number(nums[i]);
+      }
+    }
+    
+    console.log("open bracket", open_blan);
+    console.log("close bracket", close_blan);
+    var nums_final;
+    if (nums.includes("(") == true) {
+      //have bracket
+      bracket(nums)
+    } else {
+      //calculate
+      result = calculate(nums);
+    }
   }
 }
 
-function calculate() {
-  
+function calculate(content) {
+  console.log(content);
+  //determine whether a pair of bracket
+  //if (open_blan.length > 0) {
+  //  var start = 0;
+  //  var blan_count = 0;
+  //  //find out different pair of bankets.
+  //  for (var i=0; i<nums.length; i++) {
+  //    if (nums[i] == "(") {
+  //      blan_count++;
+  //    } else if (nums[i] == ")") {
+  //      blan_count--;
+  //    }
+  //
+  //    if (blan_count == 1 && nums[i] == "(") {
+  //      start = i;
+  //    }
+  //
+  //    if (blan_count == 0 && nums[i] == ")") {
+  //      nums_final = nums.toSpliced(start, i-start+1);
+  //      //bracket(nums.slice(start+1, i)).concat(nums_final);
+  //      console.log(bracket(nums.slice(start+1, i)));
+  //    }
+  //  }
+  //}
+
+
 }
+
+
+function bracket(content) {
+  //determine whether a pair of bracket
+  console.log("content", content);
+  if (content.includes("(") == true) {
+    var start = 0;
+    var blan_count = 0;
+    var todelete;
+    var content_final
+    //find out different pair of bankets.
+    for (var i=0; i<content.length; i++) {
+      if (content[i] == "(") {
+        blan_count++;
+      } else if (content[i] == ")") {
+        blan_count--;
+      }
+      
+      if (blan_count == 1 && content[i] == "(") {
+        start = i;
+      }
+      
+      if (blan_count == 0 && content[i] == ")") {
+        console.log(i, content[i]);
+        //delete content[start];
+        //delete content[i];
+        todelete = content.slice(start+1, i);
+        console.log("todelete", todelete);
+
+        //console.log(content);
+        console.log("content after delete", content);
+
+        content_front = content.slice(0, start);
+        content_end = content.slice(i+1);
+        
+        console.log("front", content_front);
+        console.log("end", content_end);
+        
+        value = bracket(todelete);
+        content_final = content_front.concat(value);
+        content_final = content_final.concat(content_end);
+        console.log("content", content_final);
+      }
+    }
+  } else {
+    if (content.includes("+") || content.includes("-") || content.includes("*") || content.includes("/")) {
+      return calculate(content);
+    } else {
+      return content;
+    }
+  }
+}
+
 
 //when type operator, mark number and operator onto "nums" and "oper"
 //num_count and oper_count add one
-//if there does not exist close blanket at the end, add a close blanket automatically when calculating.
+//if there does not exist close bracket at the end, add a close bracket automatically when calculating.
